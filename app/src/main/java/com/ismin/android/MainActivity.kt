eda684d918b58c3dbd18bfa50d08f3ddc4fdfab5
Gaëtan Maisse <gmaisse@solocal.com>
2020-10-03 17:31:07 +0200

    feat: use Bookshelf to handle creation of multiple books

--- app/src/main/java/com/ismin/android/MainActivity.kt
+++ app/src/main/java/com/ismin/android/MainActivity.kt
@@ -3,11 +3,13 @@ package com.ismin.android
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     private val createBookActivityRequestCode = 1;
+    private val TAG = MainActivity::class.simpleName
+
+    private val bookshelf = Bookshelf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,8 +24,8 @@ class MainActivity : AppCompatActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == this.createBookActivityRequestCode) {
-            val book = data?.extras?.get(CREATED_BOOK_EXTRA_KEY);
-            Toast.makeText(this, book.toString(), Toast.LENGTH_SHORT).show()
+            val book = data?.extras?.get(CREATED_BOOK_EXTRA_KEY) as Book
+            bookshelf.addBook(book)
         }
     }
 }
